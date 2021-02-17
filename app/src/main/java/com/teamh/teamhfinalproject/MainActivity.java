ae69541ca008a665b0995012ef7a42a38b622e21
ALX <62620264+warferton@users.noreply.github.com>
2021-02-17 20:41:39 +0500

    Designed sidebar menu on MainActivity. Moved main-page-only related strings to page_main_strings.xml. Moved button on main page to center and made it of an appropriate size.

--- app/src/main/java/com/teamh/teamhfinalproject/MainActivity.java
+++ app/src/main/java/com/teamh/teamhfinalproject/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.find_gift_button);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
