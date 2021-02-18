c6a00ab1bdf1b0284c1102d284172aa0af8fe2a3
Zhuldyz20 <zhuldyz.ilyassova2001@gmail.com>
2021-02-18 22:01:49 +0600

    Implemented the seek bar

--- app/src/main/java/com/teamh/teamhfinalproject/filter.java
+++ app/src/main/java/com/teamh/teamhfinalproject/filter.java
@@ -3,8 +3,13 @@ package com.teamh.teamhfinalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class filter extends AppCompatActivity {
+
+    SeekBar seekBar;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,30 @@ public class filter extends AppCompatActivity {
         setContentView(R.layout.activity_filter);
 
         try { this.getSupportActionBar().hide(); } catch (NullPointerException e){}
+
+        seekBar = (SeekBar)findViewById(R.id.seekBar_budget);
+        seekBar.setProgress(5);
+        seekBar.incrementProgressBy(5);
+        seekBar.setMax(4995);
+        textView = (TextView)findViewById(R.id.user_budget);
+
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progress = progress + 5;
+                textView.setText("$ " + String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
