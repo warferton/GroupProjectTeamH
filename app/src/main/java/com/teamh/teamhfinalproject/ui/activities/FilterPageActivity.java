a980af08511ab19ceaa3a44283988069ed85f29d
Carlos Avila <ninoavila185@gmail.com>
2021-02-21 16:58:10 -0600

    Added more content to the filter page and added a simple Nav
    I can't check if the layout runs correctly, I'll try later and make any necessary changes.

--- app/src/main/java/com/teamh/teamhfinalproject/ui/activities/FilterPageActivity.java
+++ app/src/main/java/com/teamh/teamhfinalproject/ui/activities/FilterPageActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.teamh.teamhfinalproject.R;
@@ -16,11 +18,18 @@ public class FilterPageActivity extends AppCompatActivity {
 
     SeekBar seekBar;
     TextView textView;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+
+        spinner = (Spinner) findViewById(R.id.category_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
         try { this.getSupportActionBar().hide(); }
             catch (NullPointerException e){ e.printStackTrace();}
