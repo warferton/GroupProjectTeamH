ae29a2f36046a22378de0bb820b2a9817064ed4a
ALX <62620264+warferton@users.noreply.github.com>
2021-03-20 00:13:29 +0500

    Added eBay Java Client dependency gradle.  Yaml config for eBay connection.  Sub-system structure to handle API requests. Generic class to handle products - EbayProduct. Started configuring token handling, docs are super unclear so not much done. Put placeholder code instead.
    Signed-off-by: warferton <warferton45@gmail.com>

--- app/src/main/java/com/teamh/teamhfinalproject/MainActivity.java
+++ app/src/main/java/com/teamh/teamhfinalproject/MainActivity.java
@@ -2,48 +2,54 @@ package com.teamh.teamhfinalproject;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
 import android.widget.Button;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.navigation.NavigationView;
-import com.teamh.teamhfinalproject.ui.activities.FilterPageActivity;
-import com.teamh.teamhfinalproject.ui.activities.TermsAndConditionsActivity;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
+import com.ebay.api.client.auth.oauth2.CredentialUtil;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.teamh.teamhfinalproject.api.ebay.security.EbayTokenManager;
+import com.teamh.teamhfinalproject.ui.activities.FilterPageActivity;
+import com.teamh.teamhfinalproject.ui.activities.TermsAndConditionsActivity;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
 
+    //instantiate token manager
+    private EbayTokenManager tokenManager = new EbayTokenManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /**load creds from yaml**/
+        try {
+            CredentialUtil.load(new FileInputStream("./api/ebay/ebay-config.yml"));
+            System.out.println("\n=========Successfully Loaded the Credentials =========\n");
+        } catch (FileNotFoundException e) {
+            System.err.println("Failed to Load Credentials at MainActivity.java, 40.\n");
+            e.printStackTrace();
+        }
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.find_gift_button);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openFilterActivity();
-            }
-        });
+        fab.setOnClickListener(view -> openFilterActivity());
         Button terms_button = findViewById(R.id.nav_button_terms);
-        terms_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openTermsAndConditionsActivity();
-            }
-        });
+        terms_button.setOnClickListener(view -> openTermsAndConditionsActivity());
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
